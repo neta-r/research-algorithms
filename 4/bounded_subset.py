51c76d9dd57a3cdcc5642c8243944aabc3d25aff
neta-r <neta2611@walla.com>
2022-04-02 19:26:03 +0300

    working version :)

--- 4/bounded_subset.py
+++ 4/bounded_subset.py
@@ -1,30 +1,35 @@
 class bounded_subset:
     def __init__(self, lst: list, C: int):
         self.c = C
-        self.obj = lst
-        self.start = 0
-        self.end = 1
-        self.group_size = 1
+        self.lst = lst
+        self.appear = ""
+        self.stop = "1"
+        for _ in range(len(lst)):
+            self.appear = self.appear + "0"
+            self.stop = self.stop + "0"
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.group_size > len(self.obj):
-            raise StopIteration
-        while True:
-            temp_sum = sum(self.obj[self.start: self.end])
-            temp_group = self.obj[self.start: self.end]
-            if self.start + self.group_size >= len(self.obj):
-                self.group_size = self.group_size + 1
-                self.start = 0
-                self.end = self.group_size
-            else:
-                self.start = self.start + 1
-                self.end = self.start + self.group_size
-            # if temp_sum <= self.c:
-            #     return temp_group
-            return temp_group
+        con_to_next_iter = True
+        while con_to_next_iter:
+            con_to_next_iter = False
+            self.appear = bin(int(self.appear, 2) + 1)[2:]
+            bin_str = self.appear.zfill(len(self.lst))
+            if bin_str == self.stop:
+                raise StopIteration
+            temp_sum = 0
+            temp_lst = []
+            for i in range(len(self.lst)):
+                if bin_str[i] != "0":
+                    temp_sum = temp_sum + self.lst[i]
+                    temp_lst.append(self.lst[i])
+                    if temp_sum > self.c:
+                        con_to_next_iter = True
+                        break
+            if not con_to_next_iter:
+                return temp_lst
 
 
 for s in bounded_subset([1, 2, 3, 1], 4):
