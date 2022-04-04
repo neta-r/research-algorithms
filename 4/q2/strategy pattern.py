cef7f0735c04664911547d4aec060d5a455149a5
neta-r <neta2611@walla.com>
2022-04-04 14:15:51 +0300

    finished all , need to test

--- 4/q2/strategy pattern.py
+++ 4/q2/strategy pattern.py
@@ -12,6 +12,7 @@ def partition(algorithm: Callable, dists: list, cities_names=None, out_type: str
             c = city_with_name(cities_names[i], dists[i])
             cities_list.append(c)
     else:
+        city_without_name.key = 0
         for i in range(len(dists)):
             c = city_without_name(dists[i])
             cities_list.append(c)
@@ -24,6 +25,8 @@ def partition(algorithm: Callable, dists: list, cities_names=None, out_type: str
 
 
 if __name__ == '__main__':
+
+    # names + greedy + path
     # should print : Los Angeles->New York->Brooklyn->Chicago->California
     print(partition(greedy, [{"Brooklyn": 7, "New York": 5, "California": 5, "Chicago": 10},
                              {"Los Angeles": 7, "New York": 2, "California": 11, "Chicago": 5},
@@ -31,6 +34,17 @@ if __name__ == '__main__':
                              {"Los Angeles": 5, "Brooklyn": 11, "New York": 2, "Chicago": 3},
                              {"Los Angeles": 10, "Brooklyn": 5, "New York": 4, "California": 3}],
                     ["Los Angeles", "Brooklyn", "New York", "California", "Chicago"], "path", "city_with_name"))
+
+    # names + greedy + length
+    # should print : 15
+    print(partition(greedy, [{"Brooklyn": 7, "New York": 5, "California": 5, "Chicago": 10},
+                       {"Los Angeles": 7, "New York": 2, "California": 11, "Chicago": 5},
+                       {"Los Angeles": 5, "Brooklyn": 2, "California": 2, "Chicago": 4},
+                       {"Los Angeles": 5, "Brooklyn": 11, "New York": 2, "Chicago": 3},
+                       {"Los Angeles": 10, "Brooklyn": 5, "New York": 4, "California": 3}],
+              ["Los Angeles", "Brooklyn", "New York", "California", "Chicago"], "length", "city_with_name"))
+
+    # names + brute_force + path
     # should print : Los Angeles->Brooklyn->New York->California->Chicago
     print(partition(brute_force, [{"Brooklyn": 7, "New York": 5, "California": 5, "Chicago": 10},
                                   {"Los Angeles": 7, "New York": 2, "California": 11, "Chicago": 5},
@@ -38,10 +52,32 @@ if __name__ == '__main__':
                                   {"Los Angeles": 5, "Brooklyn": 11, "New York": 2, "Chicago": 3},
                                   {"Los Angeles": 10, "Brooklyn": 5, "New York": 4, "California": 3}],
                     ["Los Angeles", "Brooklyn", "New York", "California", "Chicago"], "path", "city_with_name"))
+
+    # names + brute_force + length
+    # should print 14
+    print(partition(brute_force, [{"Brooklyn": 7, "New York": 5, "California": 5, "Chicago": 10},
+                                  {"Los Angeles": 7, "New York": 2, "California": 11, "Chicago": 5},
+                                  {"Los Angeles": 5, "Brooklyn": 2, "California": 2, "Chicago": 4},
+                                  {"Los Angeles": 5, "Brooklyn": 11, "New York": 2, "Chicago": 3},
+                                  {"Los Angeles": 10, "Brooklyn": 5, "New York": 4, "California": 3}],
+                    ["Los Angeles", "Brooklyn", "New York", "California", "Chicago"], "length", "city_with_name"))
+
+    # no names + greedy + path
     # should print: 0->2->1->4->3
     print(partition(greedy, [[7, 5, 5, 10], [7, 2, 11, 5], [5, 2, 2, 4], [5, 11, 2, 3], [10, 5, 4, 3]]
                     , out_type="path", city_type="city_without_name"))
-    # should print: 0->1->2->3->4 check!!!!!!!
+
+    # no names + greedy + length
+    # should print: 15
+    print(partition(greedy, [[7, 5, 5, 10], [7, 2, 11, 5], [5, 2, 2, 4], [5, 11, 2, 3], [10, 5, 4, 3]]
+                    , out_type="length", city_type="city_without_name"))
+
+    # no names + brute_force + path
+    # should print: 0->1->2->3->4
     print(partition(brute_force, [[7, 5, 5, 10], [7, 2, 11, 5], [5, 2, 2, 4], [5, 11, 2, 3], [10, 5, 4, 3]]
                     , out_type="path", city_type="city_without_name"))
-    # check with length only !
+
+    # no names + brute_force + length
+    # should print: 14
+    print(partition(brute_force, [[7, 5, 5, 10], [7, 2, 11, 5], [5, 2, 2, 4], [5, 11, 2, 3], [10, 5, 4, 3]]
+                    , out_type="length", city_type="city_without_name"))
