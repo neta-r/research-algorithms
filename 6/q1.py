28c5724c3897fc19cfb0893acb7300544bc17fd9
neta-r <neta2611@walla.com>
2022-05-08 14:34:17 +0300

    finished q1 :)

--- 6/q1.py
+++ 6/q1.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
@@ -5,9 +7,10 @@ import matplotlib.pyplot as plt
 
 def plotIntersection(num_range, f, g):
     results = set()
-    for i in num_range:
-        temp = fsolve(lambda x: g(x)- f(x), i)
-        results.add(round(temp[0], 1))
+    for i in range(math.ceil(num_range.min()), math.ceil(num_range.max())):
+        temp = fsolve(lambda x: f(x) - g(x), i)
+        if round(f(temp)[0], 3) == round(g(temp)[0], 3):
+            results.add(round(temp[0], 3))
     for res in results:  # plotting the intersection in red, zorder=2 will output it on top of f and g
         plt.scatter(res, f(res), c='r', zorder=3)
     plt.plot(num_range, f(num_range), zorder=2)  # plotting f, zorder=1 will output it below the roots
@@ -23,3 +26,9 @@ if __name__ == '__main__':
     g1 = lambda x: np.sin(x)
     g2 = lambda x: 0.2*x
     plotIntersection(np.linspace(-10, 10, 1000), g1, g2)
+    h1 = lambda x: np.cos(x)
+    h2 = lambda x: x+6
+    plotIntersection(np.linspace(-8, 4, 1000), h1, h2)
+    plotIntersection(np.linspace(0, 4, 1000), g1, h1)
+    plotIntersection(np.linspace(-20, 0, 1000), f2, g2)
+
